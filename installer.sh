416058a55c6cd2c6146f7ea09820f6628a850fc2
akwebsec <72439609+akwebsec@users.noreply.github.com>
2020-10-09 13:17:53 -0400

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,13 @@
+apt update 
+apt install git -y
+apt install wget -y
+apt install python -y
+apt install python3 -y
+apt install python-pip -y
+apt install python3-pip -y
+pip install requests -y
+pip3 install requests -y
+pip install colorama -y
+pip3 install colorama -y
+
+
